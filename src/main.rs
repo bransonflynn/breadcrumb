d697f08c75b0e80941031c4d9cf9e8b19116e852
bp42s <bransonyflynn@gmail.com>
2024-10-29 17:04:44 -0400

    added check for file already existing

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ fn main() {
 
 pub mod crumbs {
     use std::{fs::File, io::{self}, path::Path};
+    use whoami;
 
     pub fn on_desktop() {
         std::println!("breadcrumb::main::crumbs::on_desktop -> started...");
         
         let computer_username: String = whoami::username();
+        
         let target_path_string: String = "C:/Documents and Settings/".to_string() + &computer_username + "/Desktop/breadcrumb.txt";
-        let target_path = Path::new(&target_path_string);
-        let mut file = File::create(target_path).expect("on_desktop -> create failed");
+        let target_path: &Path = Path::new(&target_path_string);
 
-        // the text to be written
-        let text = String::from("Hello, World!");
+        if Path::exists(&target_path) {
+            std::println!("breadcrumb::main::crumbs::on_desktop -> file already exists! return");
+            return
+        }
 
+        let mut file: File = File::create(target_path).expect("on_desktop -> create failed");
+        let text: String = String::from("Hello, World!");
         io::Write::write(&mut file, text.as_bytes()).expect("on_desktop -> write failed");
         std::println!("breadcrumb::main::crumbs::on_desktop -> finished!");
     }
