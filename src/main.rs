e9c8c8c289826bddbfa74927807279aff893d6ed
bp42s <bransonyflynn@gmail.com>
2024-10-29 16:10:53 -0400

    added temp func in pubmod crumbs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    crumbs::crumbs_desktop();
+}
+
+
+
+pub mod crumbs {
+    pub fn crumbs_desktop() {
+        std::println!("desktop ran temp");
+    }
 }
